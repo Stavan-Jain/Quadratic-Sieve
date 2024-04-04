020c50b227b7b8d368433e88b1345d04da5e646d
Stavan Jain <stavansjain@gmail.com>
2024-04-04 17:20:06 -0400

    created a class called Quadratic Sieve

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 import numpy as np
 
-def factor(N, B):
-    return 
+class QuadraticSieve:
+    
+    def __init__(self, n):
+        self.n = n
+        self.matrix = np.array([[]])
+
+    #TODO: Jack
+    def find_bsmooth(self, B):
+        return 
+    
+    #TODO: Prerana
+    def get_B(self):
+        return 
+    
+    #TODO: Stavan
+    def find_nullspace(self):
+        return 4
+
+    #TODO: Prerana
+    def basic_principle(self):
+        return False
+
+Sieve = QuadraticSieve(10)
+print(Sieve.find_nullspace())
